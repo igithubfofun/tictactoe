e7dce0cab2c34828568ea71decc624202bbf6068
Metel Patel <metelpatel@gmail.com>
2015-12-16 15:29:47 -0600

    in process of choosing turns of players

--- js/main.js
+++ js/main.js
@@ -3,21 +3,66 @@ $(function(){
   var x = 'X';
   var o = 'O';
   var picked;
-
-  $('.box').on('click', function(e) {
-    picked = $('.box').val();
-    if (picked === "o"){
-      $(this).html(x);
-
-    }
-
-    else {
-      $(this).html(o);
-    }
+  var turn;
+  var playerOne = true;
+  var playerTwo = false;
 
 
-  })
+  while (playerOne === true){
 
+    $('.box').on('click', function() {
+      picked = $(this).html();
+      console.log(picked);
+      //playerOne = false;
+
+
+      if (picked === '&nbsp;'){
+          $(this).text(x);
+
+      }
+
+      //if statement for not overwritting a box
+      else if (picked === 'X' || picked === 'O'){
+        console.log("pick again");
+        return;
+
+      }
+
+      playerOne = false;
+      playerTwo = true;
+
+
+
+    })
+
+    while (playerTwo === true){
+
+    $('.box').on('click', function() {
+      picked = $(this).html();
+      console.log(picked);
+      //playerOne = false;
+
+
+      if (picked === '&nbsp;'){
+          $(this).text(x);
+
+      }
+
+      //if statement for not overwritting a box
+      else if (picked === 'X' || picked === 'O'){
+        console.log("pick again");
+        return;
+
+      }
+
+      playerOne = true;
+      playerTwo = false;;
+
+
+
+    })
+
+ // }
 
   // $('.box').on('click', function(e) {
   //   $(this).html(o);
