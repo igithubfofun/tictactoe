a33231f8573fbbce9947ba3ee29758f226ddf67e
Metel Patel <metelpatel@gmail.com>
2015-12-16 19:13:19 -0600

    refactored code without while loop

--- js/main.js
+++ js/main.js
@@ -3,76 +3,45 @@ $(function(){
   var x = 'X';
   var o = 'O';
   var picked;
-  var turn;
-  var game = true;
-  var playerOne = false;
-  var playerTwo = false;
-  var playerTurn;
+  var playerTurn = 0;
+  var p1 = [];
+  var p2 = [];
 
 
-    $('.box').on('click', function() {
-    picked = $(this).html();
-    //console.log(picked);
 
-      if (picked === '&nbsp;'){
-        $(this).text(x);
-        playerOne = true;
+  $('.box').on('click', function() {
+  picked = $(this).html();
+
+
+    if (playerTurn % 2 === 0){
+        picked = x;
+
+        var selected = $(this).attr('id');
+        p1.push(selected);
+        $(this).text(picked);
+
+
+        $(this).css('pointer-events', 'none');
+
+        if (p1.includes('one') && p1.includes('two') && p1.includes('three')) {
+          alert("WINNER");
         }
 
 
-        if (playerOne){
-        playerOne = false;
-        $('.box').on('click', function() {
-        picked = $(this).html();
-
-        if (picked === '&nbsp;'){
-            $(this).text(x);
-            }
-
-        //if statement for not overwritting a box
-        else if (picked === 'X' || picked === 'O'){
-          console.log("pick again");
-          return;
-          }
-        })
-
-
-      }
-
-
-        $('.box').on('click', function() {
-        picked = $(this).html();
-
-        if (picked === '&nbsp;'){
-            $(this).text(o);
-            }
-
-        //if statement for not overwritting a box
-        else if (picked === 'X' || picked === 'O'){
-          console.log("pick again");
-          return;
-          }
-        })
-
+        playerTurn += 1;
     }
-    })
 
+    else {
+      picked = o;
+      var selected = $(this).attr('id');
+      p2.push(selected);
+      $(this).text(picked);
 
+      $(this).css('pointer-events', 'none');
+      playerTurn += 1;
+    }
 
-
-
-
-
-
-
-  }
-
-
-
-
-
-
-
+  })
 
 
 });
